1d4296105c79b7ee53fd620b204d4bcf741657cd
harshini-sarathy <itzharshinisarathy@gmail.com>
2026-01-07 22:04:23 +0530

    Add files via upload

---
+++ practice/operators.java
@@ -0,0 +1,23 @@
+public class operators {
+	public static void main(String args[]){
+		int math = 98;
+		int phy = 99;
+		int chem = 95;
+
+		int total = math+chem+phy;
+		int avg = total/3;
+		
+		int percent = total*100/300;
+		
+		System.out.println("Mathematics: "+math);
+		System.out.println("Physics: "+phy);
+		System.out.println("Chemistry: "+chem);
+		System.out.println("Total: "+total);
+		System.out.println("Average: "+avg);
+		System.out.println("Percentage: "+percent);
+	}
+}
+
+		
+		
+		
